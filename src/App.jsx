3ef7b5a947b80950a67bcfafa62a0d04007f8842
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-11-02 15:06:07 +0700

    Setup awal

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-purple-600">
+        BERHASIL - PAKAI .JS
+      </h1>
+    </div>
+  );
+}
+export default App;
