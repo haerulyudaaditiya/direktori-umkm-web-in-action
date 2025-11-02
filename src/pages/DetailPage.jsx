df690f4cdb788477cdbec1e3cfcc1f9751795259
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-11-02 15:52:16 +0700

    feat: Setup react-router-dom dan navigasi Halaman Detail

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DetailPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Ini Halaman Detail</h1>
+      <p>Nanti data UMKM akan muncul di sini.</p>
+    </div>
+  );
+}
+
+export default DetailPage;
